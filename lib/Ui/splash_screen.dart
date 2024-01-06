d68b486c6e13c869e3636f563dd25e445561b377
Usaid Asif <usaidasif12@gmail.com>
2024-01-06 16:03:50 +0500

    Created Splash Screen

---
+++ lib/Ui/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:firebase/firebase_services/splash_services.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  SplashServices splashServices = SplashServices();
+
+  @override
+  void initState() {
+    super.initState();
+    splashServices.isLogin(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Firebase Practice',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
