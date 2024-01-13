3c4f6444a8dca88b121ad117074566f746f3d17c
Usaid Asif <usaidasif12@gmail.com>
2024-01-13 16:18:46 +0500

    Handle alreay login user

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,13 +1,26 @@
 import 'dart:async';
 
 import 'package:firebase/Ui/auth/login_screen.dart';
+import 'package:firebase/Ui/posts/post_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashServices {
   void isLogin(BuildContext context) {
-    Timer(const Duration(seconds: 3), () {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const LoginScreen()));
-    });
+    final auth = FirebaseAuth.instance;
+
+    final user = auth.currentUser;
+
+    if (user != null) {
+      Timer(const Duration(seconds: 3), () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const PostScreen()));
+      });
+    } else {
+      Timer(const Duration(seconds: 3), () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const LoginScreen()));
+      });
+    }
   }
 }
