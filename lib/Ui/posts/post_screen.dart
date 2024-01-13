ba778721389eb202009d3c67b97e1ba5bf0bf54f
Usaid Asif <usaidasif12@gmail.com>
2024-01-13 16:28:03 +0500

    Handle Logout User

--- lib/Ui/posts/post_screen.dart
+++ lib/Ui/posts/post_screen.dart
@@ -1,3 +1,6 @@
+import 'package:firebase/Ui/auth/login_screen.dart';
+import 'package:firebase/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -8,11 +11,34 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text('Post Screen'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              auth.signOut().then((value) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginScreen(),
+                  ),
+                );
+              }).onError((error, stackTrace) {
+                Utils().toastMessage(error.toString());
+              });
+            },
+            icon: Icon(Icons.logout),
+          ),
+          SizedBox(
+            width: 10,
+          )
+        ],
       ),
     );
   }
