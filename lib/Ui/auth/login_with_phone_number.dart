279194435aefebb81ee998b5edc66ea76fb30323
Usaid Asif <usaidasif12@gmail.com>
2024-01-15 18:30:55 +0500

    Created Login With PhoneNumber Screen

--- lib/Ui/auth/login_with_phone_number.dart
+++ lib/Ui/auth/login_with_phone_number.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/widgets/round_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginWithPhoneNumber extends StatefulWidget {
@@ -8,14 +9,37 @@ class LoginWithPhoneNumber extends StatefulWidget {
 }
 
 class _LoginWithPhoneNumberState extends State<LoginWithPhoneNumber> {
+  final phoneNumberController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        title: const Text(
+          'Login',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            TextFormField(
+              controller: phoneNumberController,
+              decoration: const InputDecoration(hintText: '+92 331 8712375'),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            RoundButton(title: 'Login', onTap: () {})
+          ],
+        ),
       ),
     );
   }
