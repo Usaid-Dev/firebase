92282a3437fc0fefa70c29c1d858ae3a3c0a19ec
Usaid Asif <usaidasif12@gmail.com>
2024-01-18 18:51:59 +0500

    Created Verify Screen Ui

--- lib/Ui/auth/verify_code.dart
+++ lib/Ui/auth/verify_code.dart
@@ -1,3 +1,6 @@
+import 'package:firebase/utils/utils.dart';
+import 'package:firebase/widgets/round_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class VerifyCodeScreen extends StatefulWidget {
@@ -13,14 +16,47 @@ class VerifyCodeScreen extends StatefulWidget {
 }
 
 class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
+  bool loading = false;
+
+  final phoneNumberController = TextEditingController();
+
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Verify'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        title: const Text(
+          'Verify',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            TextFormField(
+              controller: phoneNumberController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(hintText: '6 digit code'),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            RoundButton(
+              title: 'Verify',
+              loading: loading,
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
