28ca5e90c686157f401caa9485c4a6f81a2570b7
Usaid Asif <usaidasif12@gmail.com>
2024-03-20 03:32:43 +0500

    Filter the list

--- lib/Ui/posts/post_screen.dart
+++ lib/Ui/posts/post_screen.dart
@@ -18,6 +18,8 @@ class _PostScreenState extends State<PostScreen> {
 
   final ref = FirebaseDatabase.instance.ref('Post');
 
+  final searchFilter = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,49 +50,58 @@ class _PostScreenState extends State<PostScreen> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: StreamBuilder(
-              stream: ref.onValue,
-              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-                if (!snapshot.hasData) {
-                  return const CircularProgressIndicator();
-                } else {
-                  Map<dynamic, dynamic> map =
-                      snapshot.data!.snapshot.value as dynamic;
-                  List<dynamic> list = [];
-                  list.clear();
-                  list = map.values.toList();
-                  return ListView.builder(
-                    itemCount: snapshot.data!.snapshot.children.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(list[index]['title']),
-                      );
-                    },
-                  );
-                }
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TextFormField(
+              controller: searchFilter,
+              decoration: const InputDecoration(
+                hintText: "Search",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (String value) {
+                setState(() {});
               },
             ),
           ),
-          // Expanded(
-          //   child: FirebaseAnimatedList(
-          //       defaultChild: const Center(
-          //           child: Text(
-          //         'Loading',
-          //         style: TextStyle(fontSize: 50),
-          //       )),
-          //       query: ref,
-          //       itemBuilder: (context, snapshot, animation, index) {
-          //         return ListTile(
-          //           title: Text(
-          //             snapshot.child('title').value.toString(),
-          //           ),
-          //           subtitle: Text(
-          //             snapshot.child('id').value.toString(),
-          //           ),
-          //         );
-          //       }),
-          // )
+          Expanded(
+            child: FirebaseAnimatedList(
+                defaultChild: const Center(
+                    child: Text(
+                  'Loading',
+                  style: TextStyle(fontSize: 50),
+                )),
+                query: ref,
+                itemBuilder: (context, snapshot, animation, index) {
+                  final title = snapshot.child('title').value.toString();
+
+                  if (searchFilter.text.isEmpty) {
+                    return ListTile(
+                      title: Text(
+                        snapshot.child('title').value.toString(),
+                      ),
+                      subtitle: Text(
+                        snapshot.child('id').value.toString(),
+                      ),
+                    );
+                  } else if (title.toLowerCase().contains(
+                        searchFilter.text.toLowerCase().toString(),
+                      )) {
+                    return ListTile(
+                      title: Text(
+                        snapshot.child('title').value.toString(),
+                      ),
+                      subtitle: Text(
+                        snapshot.child('id').value.toString(),
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -107,3 +118,30 @@ class _PostScreenState extends State<PostScreen> {
     );
   }
 }
+
+
+ // Expanded(
+          //   child: StreamBuilder(
+          //     stream: ref.onValue,
+          //     builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+          //       if (!snapshot.hasData) {
+          //         return const CircularProgressIndicator();
+          //       } else {
+          //         Map<dynamic, dynamic> map =
+          //             snapshot.data!.snapshot.value as dynamic;
+          //         List<dynamic> list = [];
+          //         list.clear();
+          //         list = map.values.toList();
+
+          //         return ListView.builder(
+          //           itemCount: snapshot.data!.snapshot.children.length,
+          //           itemBuilder: (context, index) {
+          //             return ListTile(
+          //               title: Text(list[index]['title']),
+          //             );
+          //           },
+          //         );
+          //       }
+          //     },
+          //   ),
+          // ),
